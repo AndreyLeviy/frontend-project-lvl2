1f85c9fad65ddf86379b63364bb590f746ed100f
Andrey Leviy <left73@gmail.com>
2021-05-25 23:05:25 +0400

    добавил аргумент по умолчанию

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,7 +2,7 @@ import stylish from './stylish.js';
 import plain from './plain.js';
 import json from './json.js';
 
-const getFormatter = (format) => {
+const getFormatter = (format = stylish) => {
   const formats = { plain, json, stylish };
   return formats[format];
 };
