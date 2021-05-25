6d0e5077add7d72b7f443fa9ed1bda79dd69c367
Andrey Leviy <left73@gmail.com>
2021-05-26 00:55:53 +0400

    заменил условия if на массив

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,14 +2,9 @@ import stylish from './stylish.js';
 import plain from './plain.js';
 import json from './json.js';
 
-const getFormatter = (format) => {
-  if (format === 'plain') {
-    return plain;
-  }
-  if (format === 'json') {
-    return json;
-  }
-  return stylish;
+const getFormatter = (format = stylish) => {
+  const formats = { plain, json, stylish };
+  return formats[format];
 };
 
 export default getFormatter;
